960d7e92050a82469975f414179062d940de193b
biley.tarnkijkul <biley.tarnkijkul@thoughtworks.com>
2022-06-27 22:51:05 +0700

    create appbar

---
+++ android/app/src/main/kotlin/com/biley/grocery_market/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.biley.grocery_market
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/home_page.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class HomeScreen extends StatelessWidget {
+  final String title;
+
+  HomeScreen({required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Icon(
+          FontAwesomeIcons.basketShopping,
+          color: Colors.black,
+        ),
+        title: Text(
+          title,
+          style: TextStyle(fontSize: 20),
+        ),
+        actions: [
+          Padding(
+            padding: EdgeInsets.only(right: 15.0),
+            child: Icon(
+              FontAwesomeIcons.magnifyingGlass,
+              color: Colors.black,
+            ),
+          )
+        ],
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Container(
+                  color: Colors.black,
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// class MyHomePage extends StatefulWidget {
+//   const MyHomePage({Key? key, required this.title}) : super(key: key);
+//
+//   final String title;
+//
+//   @override
+//   State<MyHomePage> createState() => _MyHomePageState();
+// }
+//
+// class _MyHomePageState extends State<MyHomePage> {
+//   int _counter = 0;
+//
+//   void _incrementCounter() {
+//     setState(() {
+//       _counter++;
+//     });
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(widget.title),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             const Text(
+//               'You have pushed the button this many times:',
+//             ),
+//             Text(
+//               '$_counter',
+//               style: Theme.of(context).textTheme.headline4,
+//             ),
+//           ],
+//         ),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: _incrementCounter,
+//         tooltip: 'Increment',
+//         child: const Icon(Icons.add),
+//       ), // This trailing comma makes auto-formatting nicer for build methods.
+//     );
+//   }
+// }
