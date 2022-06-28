6716e66a301bf7356eea505f5ac9311b982820e9
biley.tarnkijkul <biley.tarnkijkul@thoughtworks.com>
2022-06-28 13:44:30 +0700

    finish one item in usually buy

--- lib/components/usually_buy_section.dart
+++ lib/components/usually_buy_section.dart
@@ -4,6 +4,21 @@ import 'package:grocery_market/components/section.dart';
 class UsuallyBuySection extends StatelessWidget {
   var usually_buy = [
     Container(
+      width: 150,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.all(color: Color.fromRGBO(242, 242, 242, 1), width: 2),
+        borderRadius: BorderRadius.all(
+          Radius.circular(30),
+        ),
+        // boxShadow: <BoxShadow>[
+        //   BoxShadow(
+        //     color: Colors.black54,
+        //     blurRadius: 1.0,
+        //     offset: Offset(0.0, 0.75),
+        //   )
+        // ],
+      ),
       child: Column(
         children: [
           Image(
