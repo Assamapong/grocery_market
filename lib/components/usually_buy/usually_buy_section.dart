b9407a267f7755b197d3896cefd02e5a72d6e3c0
biley.tarnkijkul <biley.tarnkijkul@thoughtworks.com>
2022-06-29 12:32:57 +0700

    fix add product function to be duplicate when select product from usually-buy then go to order then add again

--- lib/components/usually_buy/usually_buy_section.dart
+++ lib/components/usually_buy/usually_buy_section.dart
@@ -1,30 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:grocery_market/components/section.dart';
+import 'package:grocery_market/model/product_list.dart';
 import 'item_card.dart';
 
 class UsuallyBuySection extends StatelessWidget {
-  var usually_buy = [
-    ItemCard(
-      product: {
-        "itemId": 6,
-        "itemName": 'Salmon',
-        "itemSubtitle": "1kg/235B",
-        "itemImage": AssetImage("images/salmon-fillet.jpeg"),
-        "category": "Fish"
-      },
-    ),
-    ItemCard(
-      product: {
-        "itemId": 4,
-        "itemName": 'Asparagus',
-        "itemSubtitle": "1kg/25B",
-        "itemImage": AssetImage("images/asparagus.jpeg"),
-        "category": "Vegetables"
-      },
-    )
-  ];
+  // var usually_buy = [
+  //   ItemCard(
+  //     product: {
+  //       "itemId": 6,
+  //       "itemName": 'Salmon',
+  //       "itemSubtitle": "1kg/235B",
+  //       "itemImage": AssetImage("images/salmon-fillet.jpeg"),
+  //       "category": "Fish"
+  //     },
+  //   ),
+  //   ItemCard(
+  //     product: {
+  //       "itemId": 4,
+  //       "itemName": 'Asparagus',
+  //       "itemSubtitle": "1kg/25B",
+  //       "itemImage": AssetImage("images/asparagus.jpeg"),
+  //       "category": "Vegetables"
+  //     },
+  //   )
+  // ];
+
+  List<Widget> getUsuallyBuyProducts() {
+    List<Widget> usually_buy = [];
+    productList.where((product) => product["count"] > 8).forEach((product) {
+      usually_buy.add(ItemCard(product: product));
+    });
+    return usually_buy;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Section(sectionTitle: 'Usually buy', sectionChild: usually_buy);
+    return Section(
+        sectionTitle: 'Usually buy', sectionChild: getUsuallyBuyProducts());
   }
 }
