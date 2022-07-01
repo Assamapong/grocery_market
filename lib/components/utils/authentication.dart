d7c20a2ef4ad78e0fc497f0208eb25411088264e
assamapong <assamapong.t@gmail.com>
2022-07-01 12:52:33 +0700

    refactor and create authentication class

---
+++ lib/components/utils/authentication.dart
@@ -0,0 +1,57 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../user.dart';
+
+class Authentication {
+  static Future<dynamic> loginWithGoogle() async {
+    final googleSignIn = GoogleSignIn(scopes: ['email']);
+    try {
+      final googleSignInAccount = await googleSignIn.signIn();
+      if (googleSignInAccount == null) {
+        return null;
+      }
+
+      final googleSignInAuthentication =
+          await googleSignInAccount.authentication;
+      final credential = GoogleAuthProvider.credential(
+          accessToken: googleSignInAuthentication.accessToken,
+          idToken: googleSignInAuthentication.idToken);
+      await FirebaseAuth.instance.signInWithCredential(credential);
+
+      // await FirebaseFirestore.instance.collection('user').add({
+      //   'email': googleSignInAccount.email,
+      //   'imageUrl': googleSignInAccount.photoUrl,
+      //   'name': googleSignInAccount.displayName
+      // });
+      return googleSignInAccount;
+    } on FirebaseAuthException catch (e) {
+      var content = '';
+      switch (e.code) {
+        case 'account-exists-with-different-credential':
+          content:
+          'This account exists with a different sign in provider';
+          break;
+        case 'invalid-credential':
+          content:
+          'Unknown error has occurred';
+          break;
+        case 'operation-not-allowed':
+          content:
+          'This operation is not allowed';
+          break;
+        case 'user-disabled':
+          content:
+          'The user you tried to log into is disabled';
+          break;
+        case 'user-not-found':
+          content:
+          'The user you tried to log into was not found';
+          break;
+      }
+      print(content);
+    }
+  }
+}
--- lib/screens/signin_screen.dart
+++ lib/screens/signin_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:grocery_market/components/UI/login_button.dart';
+import 'package:grocery_market/components/utils/authentication.dart';
 import 'package:grocery_market/screens/signup_screen.dart';
 import 'package:grocery_market/screens/main_menu.dart';
 import 'package:grocery_market/user.dart';
@@ -21,97 +22,109 @@ class SigninScreen extends StatefulWidget {
 class _SigninScreenState extends State<SigninScreen> {
   var loading = false;
   void _loginWithGoogle() async {
-    final googleSignIn = GoogleSignIn(scopes: ['email']);
-    try {
-      final googleSignInAccount = await googleSignIn.signIn();
-      if (googleSignInAccount == null) {
-        return;
-      }
-      setState(() {
-        loading = true;
-      });
-      final googleSignInAuthentication =
-          await googleSignInAccount.authentication;
-      final credential = GoogleAuthProvider.credential(
-          accessToken: googleSignInAuthentication.accessToken,
-          idToken: googleSignInAuthentication.idToken);
-      await FirebaseAuth.instance.signInWithCredential(credential);
-      Provider.of<LoggedInUser>(context, listen: false)
-          .setUser(googleSignInAccount);
-      // await FirebaseFirestore.instance.collection('user').add({
-      //   'email': googleSignInAccount.email,
-      //   'imageUrl': googleSignInAccount.photoUrl,
-      //   'name': googleSignInAccount.displayName
-      // });
-      Navigator.of(context).pushAndRemoveUntil(
-          MaterialPageRoute(
-            builder: (builder) => MainMenu(
-              title: 'GroceryMarket',
-            ),
+    Provider.of<LoggedInUser>(context, listen: false)
+        .setUser(await Authentication.loginWithGoogle());
+    Navigator.of(context).pushAndRemoveUntil(
+        MaterialPageRoute(
+          builder: (builder) => MainMenu(
+            title: 'GroceryMarket',
           ),
-          (route) => false);
-    } on FirebaseAuthException catch (e) {
-      var content = '';
-      switch (e.code) {
-        case 'account-exists-with-different-credential':
-          content:
-          'This account exists with a different sign in provider';
-          break;
-        case 'invalid-credential':
-          content:
-          'Unknown error has occurred';
-          break;
-        case 'operation-not-allowed':
-          content:
-          'This operation is not allowed';
-          break;
-        case 'user-disabled':
-          content:
-          'The user you tried to log into is disabled';
-          break;
-        case 'user-not-found':
-          content:
-          'The user you tried to log into was not found';
-          break;
-      }
-      showDialog(
-        context: context,
-        builder: (context) => AlertDialog(
-          title: Text('Login with google failed'),
-          content: Text(content),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: Text('Ok'),
-            )
-          ],
         ),
-      );
-    } catch (e) {
-      print(e);
-      showDialog(
-        context: context,
-        builder: (context) => AlertDialog(
-          title: Text('Login with google failed'),
-          content: Text("An unknown error occurred"),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: Text('Ok'),
-            )
-          ],
-        ),
-      );
-    } finally {
-      setState(() {
-        loading = false;
-      });
-    }
+        (route) => false);
   }
+  // void _loginWithGoogle() async {
+  //   final googleSignIn = GoogleSignIn(scopes: ['email']);
+  //   try {
+  //     final googleSignInAccount = await googleSignIn.signIn();
+  //     if (googleSignInAccount == null) {
+  //       return;
+  //     }
+  //
+  //     setState(() {
+  //       loading = true;
+  //     });
+  //     final googleSignInAuthentication =
+  //         await googleSignInAccount.authentication;
+  //     final credential = GoogleAuthProvider.credential(
+  //         accessToken: googleSignInAuthentication.accessToken,
+  //         idToken: googleSignInAuthentication.idToken);
+  //     await FirebaseAuth.instance.signInWithCredential(credential);
+  //     Provider.of<LoggedInUser>(context, listen: false)
+  //         .setUser(googleSignInAccount);
+  //     // await FirebaseFirestore.instance.collection('user').add({
+  //     //   'email': googleSignInAccount.email,
+  //     //   'imageUrl': googleSignInAccount.photoUrl,
+  //     //   'name': googleSignInAccount.displayName
+  //     // });
+  //     Navigator.of(context).pushAndRemoveUntil(
+  //         MaterialPageRoute(
+  //           builder: (builder) => MainMenu(
+  //             title: 'GroceryMarket',
+  //           ),
+  //         ),
+  //         (route) => false);
+  //   } on FirebaseAuthException catch (e) {
+  //     var content = '';
+  //     switch (e.code) {
+  //       case 'account-exists-with-different-credential':
+  //         content:
+  //         'This account exists with a different sign in provider';
+  //         break;
+  //       case 'invalid-credential':
+  //         content:
+  //         'Unknown error has occurred';
+  //         break;
+  //       case 'operation-not-allowed':
+  //         content:
+  //         'This operation is not allowed';
+  //         break;
+  //       case 'user-disabled':
+  //         content:
+  //         'The user you tried to log into is disabled';
+  //         break;
+  //       case 'user-not-found':
+  //         content:
+  //         'The user you tried to log into was not found';
+  //         break;
+  //     }
+  //     showDialog(
+  //       context: context,
+  //       builder: (context) => AlertDialog(
+  //         title: Text('Login with google failed'),
+  //         content: Text(content),
+  //         actions: [
+  //           TextButton(
+  //             onPressed: () {
+  //               Navigator.of(context).pop();
+  //             },
+  //             child: Text('Ok'),
+  //           )
+  //         ],
+  //       ),
+  //     );
+  //   } catch (e) {
+  //     print(e);
+  //     showDialog(
+  //       context: context,
+  //       builder: (context) => AlertDialog(
+  //         title: Text('Login with google failed'),
+  //         content: Text("An unknown error occurred"),
+  //         actions: [
+  //           TextButton(
+  //             onPressed: () {
+  //               Navigator.of(context).pop();
+  //             },
+  //             child: Text('Ok'),
+  //           )
+  //         ],
+  //       ),
+  //     );
+  //   } finally {
+  //     setState(() {
+  //       loading = false;
+  //     });
+  //   }
+  // }
 
   @override
   Widget build(BuildContext context) {
