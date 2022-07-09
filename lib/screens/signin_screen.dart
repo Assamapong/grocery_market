c4088014a5118902f2fb985d8d81bf80c8dd705e
biley.tarnkijkul <biley.tarnkijkul@thoughtworks.com>
2022-07-10 01:56:53 +0700

    adjust sign in screen

--- lib/screens/signin_screen.dart
+++ lib/screens/signin_screen.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+import 'dart:math';
+
+import 'package:crypto/crypto.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
@@ -6,6 +10,7 @@ import 'package:grocery_market/components/SocialButton.dart';
 import 'package:grocery_market/screens/main_menu.dart';
 import 'package:grocery_market/screens/signup_screen.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:sign_in_with_apple/sign_in_with_apple.dart';
 
 class SigninScreen extends StatelessWidget {
   final String title;
@@ -41,6 +46,49 @@ class SigninScreen extends StatelessWidget {
 
       // Once signed in, return the UserCredential
       return FirebaseAuth.instance.signInWithCredential(facebookAuthCredential);
+    }
+
+    String generateNonce([int length = 32]) {
+      final charset =
+          '0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._';
+      final random = Random.secure();
+      return List.generate(
+          length, (_) => charset[random.nextInt(charset.length)]).join();
+    }
+
+    /// Returns the sha256 hash of [input] in hex notation.
+    String sha256ofString(String input) {
+      final bytes = utf8.encode(input);
+      final digest = sha256.convert(bytes);
+      return digest.toString();
+    }
+
+    Future<UserCredential> signInWithApple() async {
+      // To prevent replay attacks with the credential returned from Apple, we
+      // include a nonce in the credential request. When signing in with
+      // Firebase, the nonce in the id token returned by Apple, is expected to
+      // match the sha256 hash of `rawNonce`.
+      final rawNonce = generateNonce();
+      final nonce = sha256ofString(rawNonce);
+
+      // Request credential for the currently signed in Apple account.
+      final appleCredential = await SignInWithApple.getAppleIDCredential(
+        scopes: [
+          AppleIDAuthorizationScopes.email,
+          AppleIDAuthorizationScopes.fullName,
+        ],
+        nonce: nonce,
+      );
+
+      // Create an `OAuthCredential` from the credential returned by Apple.
+      final oauthCredential = OAuthProvider("apple.com").credential(
+        idToken: appleCredential.identityToken,
+        rawNonce: rawNonce,
+      );
+
+      // Sign in the user with Firebase. If the nonce we generated earlier does
+      // not match the nonce in `appleCredential.identityToken`, sign in will fail.
+      return await FirebaseAuth.instance.signInWithCredential(oauthCredential);
     }
 
     return Scaffold(
@@ -122,7 +170,7 @@ class SigninScreen extends StatelessWidget {
                     }),
                 SocialButton(
                     buttonColor: Colors.blue,
-                    buttonLogo: AssetImage('images/google_logo.png'),
+                    buttonLogo: AssetImage('images/facebook_logo.png'),
                     buttonText: 'Login with Facebook',
                     ontap: () async {
                       await signInWithFacebook();
@@ -134,11 +182,6 @@ class SigninScreen extends StatelessWidget {
                           ),
                           (route) => false);
                     }),
-                SocialButton(
-                    buttonColor: Colors.green,
-                    buttonLogo: AssetImage('images/google_logo.png'),
-                    buttonText: 'Login with Google',
-                    ontap: () {})
               ],
             ),
           )
